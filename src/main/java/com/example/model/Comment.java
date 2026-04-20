fa8d17497adf37361e3c77d23efe723bf1403c7b
Ricardo A <fma.7r5@gmail.com>
2026-04-19 20:49:09 -0600

    added ignore Json to avoid getting the fields when calling comments endpoint

--- src/main/java/com/example/model/Comment.java
+++ src/main/java/com/example/model/Comment.java
@@ -1,7 +1,16 @@
 package com.example.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Comment {
@@ -15,10 +24,12 @@ public class Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
+    @JsonIgnore
     private Product product;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private Users user;
 
     // Constructors
